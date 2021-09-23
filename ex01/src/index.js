b0f119b03b8ed21d730fea6836426bb10cbfe119
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-23 22:30:46 +0200

    aDDED index.js

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,20 @@
+// Create an array here
 
+var myArr = [2,4,0,8,10]
+
+// End of creating an array
+
+function myArrayFunction(arr) {
+    var myItems = [...arr];
+        // Only change code below this line
+
+        myItems[2] = 6;
+
+        return myItems;
+
+        // Only change code above this line
+
+}
+
+myArrayFunction(myArr);
+module.exports = myArrayFunction;
