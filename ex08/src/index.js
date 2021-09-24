95937888be44d666e0045d19c340327cc2925d31
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-24 05:01:33 +0200

    added index.js

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,1 +1,18 @@
+// Only change code below this line
+function myMutation(arr) {
 
+    var alphabetletters = arr[0].toLowerCase();
+    var allletters = arr[1].toLowerCase();
+
+    for(let i = 0; i < allletters.length; i++){
+        if(alphabetletters.includes(allletters[i]) == false){
+            return false;
+        }
+    }
+    return true;
+}
+
+// Only change code above this line
+
+console.log(myMutation(["Alien", "line"]));
+module.exports = myMutation;
