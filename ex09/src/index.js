7b3af146d47e7b81d3a2bc95ab6ce82156a1e84b
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-24 05:04:24 +0200

    added index.js

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,1 +1,22 @@
+// Only change code below this line
+function myBouncer(arr){
 
+    var otherArr = [...arr];
+    var truthArr = [];
+    for(let i = 0; i < otherArr.length; i++){
+        if(otherArr[i]){
+            truthArr.push(otherArr[i]);
+        }
+    }
+
+    return truthArr;
+
+}
+
+// Only change code above this line
+
+console.log(myBouncer([7, "ate", "", false, 9]));
+console.log(myBouncer(["a", "b", "c"]));
+console.log(myBouncer([false, null, 0, NaN, undefined, ""]));
+console.log(myBouncer([null, NaN, 1, 2, undefined]));
+module.exports = myBouncer;
