88cfbb87c1f04c4d99974c734fcd71922a52e97c
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-24 04:52:27 +0200

    added index.js

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,1 +1,22 @@
+// Only change code below this line
+function multiplyArrayFunction(myArray){
 
+    myOtherArray = [...myArray];
+    var sum = 0;
+    var product = 1;
+    for(let i = 0; i < myOtherArray.length; i++){
+        for(let j = 0; j <myOtherArray[i].length; j++){
+            sum = sum + myOtherArray[i][j];
+            product = product * myOtherArray[i][j];
+        }
+    }
+    var arr = [product, sum];
+    return arr;
+}
+
+
+// Only change code above this line
+console.log(multiplyArrayFunction([[2], [5, 6, 7], [8, 9]]));
+console.log(multiplyArrayFunction([[2.5, 2], [0.5, 0.2], [8]]))
+console.log(multiplyArrayFunction([[1, 2], [3, 4, 5, 6], [7, 8, 9]]))
+module.exports = multiplyArrayFunction;
