06ea535a980212378f1c0853d190b8fc744ee1dc
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-24 04:56:32 +0200

    added index.js

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,1 +1,22 @@
+// Only change code below this line
+function largestNumFromArray(arr) {
 
+    var myArr = [...arr];
+    var maxNumArray = [];
+    for(let i = 0; i < myArr.length; i++){
+        var max = myArr[i][0];
+        for(let j = 1; j < myArr.length; j++){
+            if ( max < myArr[i][j]){
+                max = myArr[i][j];
+            }
+        }
+        maxNumArray.push(max);
+    }
+    return maxNumArray;
+}
+// Only change code above this line
+
+console.log(largestNumFromArray([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+console.log(largestNumFromArray([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 875, 1]]));
+console.log(largestNumFromArray([[17, 23, 25, 12], [25, 7, 34, 48], [4, 10, 18, 21], [72, 3, 17, 10]]));
+module.exports = largestNumFromArray;
