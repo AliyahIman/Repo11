08d7a0640a266d2e4e6d7d7b7d34a0166d575c17
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-24 04:46:09 +0200

    added index.js

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,1 +1,23 @@
+// Create a monitorsListArray array here
 
+var monitorsListArray = ["Apple", 1, "Peach", "Berry"];
+
+// End of monitorsListArray array
+
+function myMonitorsFunction(arr){
+
+    var theMonitorsList = [...arr];
+    // Only change code below this line
+
+    var monitorsList = [];
+    for(let i = 0; i < theMonitorsList.length; i++){
+        monitorsList.push(theMonitorsList[i]);
+        monitorsList.push(i+1);
+    }
+    return monitorsList;
+
+    // Only change code above this line
+}
+
+console.log(myMonitorsFunction(monitorsListArray));
+module.exports = myMonitorsFunction;
